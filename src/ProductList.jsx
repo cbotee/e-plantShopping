4055aab31dba5375619a8411a8d7da7c59d577e4
V A <cbotee@proton.me>
2025-01-29 03:45:36 -0500

    implemented addItem(), removeItem() amd updateQuantity() functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
-import { addItem } from './CartSlice';
 import CartItem from './CartItem';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
